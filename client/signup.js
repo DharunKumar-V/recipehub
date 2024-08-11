b0b5ddf3d9496f68fb95774fde6e2efd83a06273
DharunKumar-V <dharunkumar359@gmail.com>
2024-08-11 23:51:25 +0530

    index url

--- client/signup.js
+++ client/signup.js
@@ -8,7 +8,7 @@ document.getElementById("userForm").addEventListener("submit",async function(eve
     console.log("form submitted");  
   
   try{
-    const response=await fetch("https:localhost:5000/sign-in",
+    const response=await fetch("http://localhost:5000/sign-in",
       {
         method:"POST",
         mode:"cors",
