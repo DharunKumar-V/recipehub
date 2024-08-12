d9d921d116bca3f4d1c75114bbdc7321e55457bc
DharunKumar-V <dharunkumar359@gmail.com>
2024-08-12 07:58:37 +0530

    done

--- client/signup.js
+++ client/signup.js
@@ -8,7 +8,7 @@ document.getElementById("userForm").addEventListener("submit",async function(eve
     console.log("form submitted");  
   
   try{
-    const response=await fetch("http://recipehub-final.vercel.app/sign-in",
+    const response=await fetch("https://recipehub-final.vercel.app/sign-in",
       {
         method:"POST",
         mode:"cors",
